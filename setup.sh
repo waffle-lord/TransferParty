e7a243272d97902af0cb6f32c056f527c3c112c7
waffle-lord <76401815+waffle-lord@users.noreply.github.com>
2026-05-21 11:04:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,30 +10,39 @@ if [ -f ".no-setup" ]; then
 fi
 
 # add required packages
+echo ">>> adding required packages"
 apk add abuild alpine-conf syslinux xorriso squashfs-tools grub mtools
 
 # setup community repo
-setup-apkrepos -c
+echo ">>> adding community repo"
+1 | setup-apkrepos -c
 
 # add nano and neovim editors. Options are nice :)
+echo ">>> adding nano and neovim"
 apk add neovim nano
 
 # add build user and add them to the abuild group
+echo ">>> adding build user"
 adduser build -D
 adduser build abuild
 
-# copy data to build user
-cp -R TransferParty/* /home/build
-chown build:build -R /home/build/
-
-# login to build user
-su - build
-
 # setup tmp directory
-mkdir -pv ~/tmp
-echo "export TMPDIR=~/tmp" > .profile
+echo ">>> adding build user tmp dir"
+mkdir -pv /home/build/tmp
+echo "export TMPDIR=~/tmp" > /home/build/.profile
+
+# copy data to build user
+echo ">>> copying transferparty data"
+cp -R ~/TransferParty/* /home/build
 
 # clone aports repo
-git clone --depth=1 https://github.com/alpinelinux/aports.git
+echo ">>> cloning aports repo"
+git clone --depth=1 https://github.com/alpinelinux/aports.git /home/build/aports
+
+# make sure build owns its files
+echo ">>> updating build ownership"
+chown build:build -R /home/build/
 
 touch .no-setup
+
+echo ">>> done"
